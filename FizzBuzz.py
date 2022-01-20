9a6711580db65f0d28a0532d14065b362abf4843
Sahana Basapathi <srsahana67@gmail.com>
2022-01-20 14:29:01 -0500

    Basic Workshop HW0

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+#@author: Sahana Basapathi
+
+def FizzBuzz():
+    for i in range(1,21):
+        if i%3==0 and i%5==0:
+            print("FizzBuzz")
+        elif i%3==0:
+            print("Fizz")
+        elif i%5==0:
+            print("Buzz")
+        else:
+            print(i)
+
+FizzBuzz()
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Basics
-CS5704 Software Engineering Basics Workshop: HW0 
+CS5704 Software Engineering Basics Workshop: HW0
+
+Sahana Basapathi
+Hello world!
+
+Stand up meeting completed with Pratik, email: pratikn@vt.edu
